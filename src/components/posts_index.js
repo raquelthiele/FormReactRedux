a6620c78940f3720a20e59e8da64d4ca7c63364c
raquelthiele <raquelthiele@hotmail.com>
2018-08-21 14:52:58 -0300

    Create actions

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from '../actions/index';
+import { fetchPosts } from '../actions';
 import { Link } from 'react-router';
 
 class PostsIndex extends Component {
